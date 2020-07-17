4ed239acd111ec04345bf1adacd9c24e67648812
Khwilo Kabaka <khwilowatai@gmail.com>
2020-07-17 12:42:04 +0300

    Add services section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import Banner from './components/Banner';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main className='main'>
         <Banner />
+        <Services />
       </main>
       <footer className='footer'></footer>
     </div>
